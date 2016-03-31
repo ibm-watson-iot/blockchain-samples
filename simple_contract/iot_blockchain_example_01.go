01b439456f1e74b5bb219e07186d3918881806db
kletkeman <kim@letkeman.net>
2016-03-31 11:42:59 -0400

    tweak copyright

--- simple_contract/iot_blockchain_example_01.go
+++ simple_contract/iot_blockchain_example_01.go
@@ -15,6 +15,7 @@ See the License for the specific language governing permissions and limitations 
 Contributors:
 
 Sumabala Nair - Initial Contribution
+Kim Letkeman - Initial Contribution
 
 ******************************************************************************/
 //SN: March 2016
