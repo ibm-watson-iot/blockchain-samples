a6d9fdb0dd8d1f0145dc039534762d940f6fceeb
sumanair <sumanair@users.noreply.github.com>
2016-05-12 10:05:46 -0400

    Update samples.go

--- simple_contract_hyperledger/samples.go
+++ simple_contract_hyperledger/samples.go
@@ -11,6 +11,10 @@ var samples = `
         },
         "temperature": 123.456
     },
+    "initEvent": {
+        "nickname": "SIMPLE",
+        "version": "The ID of a managed asset. The resource focal point for a smart contract."
+    },
     "state": {
         "assetID": "The ID of a managed asset. The resource focal point for a smart contract.",
         "carrier": "transport entity currently in possession of asset",
